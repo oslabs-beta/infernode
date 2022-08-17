3395b19d56f3e85ce5202b959884fe61c1598fbe
dzidupe <ekdzidupe@gmail.com>
2022-08-17 18:08:42 -0400

    Commented out readiness check, added .gitkeep files, adjusted the .gitignore for the database directory and commented the captureModel

--- src/models/captureModel.ts
+++ src/models/captureModel.ts
@@ -1,33 +1,25 @@
 import sqlite3, { Database } from 'sqlite3';
 import path from 'path';
 import fs from 'fs';
-//Captures stack traces when handling queries. Makes it easier to debug.
 
-// If DB file already exists
-//   try to open it
-//   catch if it can't and error
-//   check if there is already a capture table
-//   if not, error
-// If DB file does not exist
-//   try to create it
-//   catch if it can't and error
-//   try to create a capture table
-//   if it can't, error
+//Captures stack traces when handling queries. Makes it easier to debug errors resulting from bad queries.
 sqlite3.verbose();
-const dbPath: string = path.resolve(__dirname, '../../database/captureInfo.db')
+
+const dbPath: string = path.resolve(__dirname, '../../database/captureDB.db')
 console.log(`SQLite3 DB path: ${dbPath}`);
 
+//Creating variable for capture database
 let captureDB: Database;
+
+//Checking if db file path already exists
 let dbExists: boolean = fs.existsSync(dbPath);
 
-if (dbExists) {
-  console.log('Using existing DB');
-}
-else {
-  console.log('No existing DB, creating new DB');
-}
+//For info on the node sqlite3 API and what the methods do:
+//https://github.com/TryGhost/node-sqlite3/wiki/API
 
-captureDB = new sqlite3.Database(dbPath, sqlite3.OPEN_READWRITE, (err) => {
+//Creating a new instance of the sqlite database
+captureDB = new sqlite3.Database(dbPath, sqlite3.OPEN_READWRITE | sqlite3.OPEN_CREATE, (err) => {
+  //Console logs to confirm connection to the database
   if (err) {
     console.log("Error occurred when connecting to the database: ", err);
   }
@@ -36,22 +28,26 @@ captureDB = new sqlite3.Database(dbPath, sqlite3.OPEN_READWRITE, (err) => {
   }
 });
 
+//If the db was just created, create a table for the capture information
 if (!dbExists) {
   console.log('Creating capture table in newly created DB');
-  //JSON can be stored as text in SQLite. Currently requiring data field, so if no data is needed,
-  //we can always convert an empt JSON object to a string
-  //If it turns out we don't really need the data field, we can also get rid of it. 
-  let createTableSQL: string = 'CREATE TABLE capture(id INT NOT NULL , capture_name TEXT NOT ';
+  
+  let createTableSQL: string = 'CREATE TABLE capture(id INT NOT NULL, capture_name TEXT NOT ';
   createTableSQL += 'NULL, date, creator TEXT NOT NULL, app_name TEXT NOT NULL, data';
   createTableSQL += ' TEXT NOT NULL)'
+
   captureDB.run(createTableSQL, (err) => {
     if (err) {
       console.log('Error occurred when creating a table in the database.');
     }
   });
-} else {
+}
+//If the db already exists, console log the contents
+//Might need to add checks to make sure that the table is formatted correctly
+else {
   console.log('Using existing capture table: ');
   captureDB.all(`SELECT * FROM capture`, (err, rows) => {
+    //console logging the existing rows in the database
     rows.forEach(el => {
       console.log(el);
     })
