be0573e16b27154ad2a5f31a39c431f8a62a288c
Will Mack <wyllyammack@gmail.com>
2022-08-18 18:50:00 -0500

    feature/filecontroller Added filecontroller.addData

--- src/controllers/file.controller.ts
+++ src/controllers/file.controller.ts
@@ -1,37 +1,51 @@
 import { Request, Response, NextFunction } from 'express';
-import { join } from 'path';
-import { readFileSync, writeFileSync, promises as fsPromises } from 'fs';
+import fs from 'fs-extra';
+import formidable from 'formidable';
+import path from 'path';
 
 export default class FileController {
   constructor() {}
+
   addData = async (req: Request, res: Response, next: NextFunction) => {
-    console.table(req.body);
-    const profiledData: string = String(req.body);
+    // Get next ID
     const fileId: number = res.locals.id;
-    /**
-     * flags:
-     *  - w = Open file for reading and writing. File is created if not exists
-     *  - a+ = Open file for reading and appending. The file is created if not exists
-     */
-    try {
-      // await writing the new file, first argument is file location, second argument is data used to populate the new file
-      await fsPromises.writeFile(
-        join(__dirname, '../../database/captures/', `${fileId}`),
-        profiledData,
-        {
-          flag: 'w',
-        }
+
+    const processTempFile = (formName: string, file: formidable.File) => {
+      console.log(
+        `${new Date().toLocaleString()}: Parsed POST'd file, formname ${JSON.stringify(
+          formName
+        )}, files ${JSON.stringify(file)}`
       );
-      return next();
-    } catch (err) {
-      //do error handler things
-      console.log(err);
-      return next({
-        message: 'Error in the FileController.addData middleware',
-      });
-    }
+      let filePath: string = file.filepath;
+      const currentPath = filePath; //files.capture.filepath;
+      const renamedPath = path.resolve(
+        __dirname,
+        '../../database/uploads',
+        `${fileId}.perf`
+      );
+      const destinationPath = path.resolve(
+        __dirname,
+        '../../database/captures/',
+        `${fileId}.perf`
+      );
+
+      try {
+        fs.renameSync(currentPath, renamedPath);
+        console.log('Data File successfully renamed with Id');
+        fs.move(renamedPath, destinationPath);
+        console.log('Data File successfully moved from uploads to captures');
+      } catch (err) {
+        return next(err);
+      }
+    };
+
+    const uploads = new formidable.IncomingForm({
+      uploadDir: path.resolve(__dirname, '../../database/uploads/'),
+    });
+    uploads.on('file', (fieldName: string, file: formidable.File) => {
+      processTempFile(fieldName, file);
+    });
+
+    uploads.parse(req);
   };
 }
-
-//database/captures and /database/svgs
-
