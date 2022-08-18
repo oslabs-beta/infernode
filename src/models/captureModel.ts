e486f1ef4c9467f7ed3908ea8557018bd67b08d3
Chase Walters <chasew@fastmail.com>
2022-08-18 11:34:19 -0600

    Revert tslint changes to captureModel.ts

--- src/models/captureModel.ts
+++ src/models/captureModel.ts
@@ -2,31 +2,31 @@ import sqlite3, { Database } from 'sqlite3';
 import path from 'path';
 import fs from 'fs';
 
-// Captures stack traces when handling queries. Makes it easier to debug errors resulting from
-// bad queries.
+//Captures stack traces when handling queries. Makes it easier to debug errors resulting from 
+//bad queries.
 sqlite3.verbose();
 
 const dbPath: string = path.resolve(__dirname, '../../database/captureDB.db')
 console.log(`SQLite3 DB path: ${dbPath}`);
 
-// Creating variable for capture database
+//Creating variable for capture database
 let captureDB: Database;
 
-// Checking if db file path already exists
-const dbExists: boolean = fs.existsSync(dbPath);
+//Checking if db file path already exists
+let dbExists: boolean = fs.existsSync(dbPath);
 
-// For info on the node sqlite3 API and what the methods do:
-// https://github.com/TryGhost/node-sqlite3/wiki/API
+//For info on the node sqlite3 API and what the methods do:
+//https://github.com/TryGhost/node-sqlite3/wiki/API
 
-// Creating a new instance of the sqlite database
+//Creating a new instance of the sqlite database
 captureDB = new sqlite3.Database(
-  dbPath,
+  dbPath, 
   // tslint:disable-next-line:no-bitwise
-  sqlite3.OPEN_READWRITE | sqlite3.OPEN_CREATE,
+  sqlite3.OPEN_READWRITE | sqlite3.OPEN_CREATE, 
   (err) => {
-    // Console logs to confirm connection to the database
+    //Console logs to confirm connection to the database
     if (err) {
-      console.log('Error occurred when connecting to the database: ', err);
+      console.log("Error occurred when connecting to the database: ", err);
     }
     else {
       console.log('Connected to database');
@@ -34,10 +34,10 @@ captureDB = new sqlite3.Database(
   }
 );
 
-// If the db was just created, create a table for the capture information
+//If the db was just created, create a table for the capture information
 if (!dbExists) {
   console.log('Creating capture table in newly created DB');
-
+  
   let createTableSQL: string = 'CREATE TABLE capture(id INT NOT NULL, capture_name TEXT NOT ';
   createTableSQL += 'NULL, date, creator TEXT NOT NULL, app_name TEXT NOT NULL, data';
   createTableSQL += ' TEXT NOT NULL)'
@@ -48,12 +48,12 @@ if (!dbExists) {
     }
   });
 }
-// If the db already exists, console log the contents
-// Might need to add checks to make sure that the table is formatted correctly
+//If the db already exists, console log the contents
+//Might need to add checks to make sure that the table is formatted correctly
 else {
   console.log('Using existing capture table: ');
   captureDB.all(`SELECT * FROM capture`, (err, rows) => {
-    // console logging the existing rows in the database
+    //console logging the existing rows in the database
     rows.forEach(el => {
       console.log(el);
     })
