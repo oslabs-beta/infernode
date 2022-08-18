67483869dc8e3245320a0bed8c3ed4c4d87d193f
Luke Roberts <luke.roberts089@gmail.com>
2022-08-18 12:50:22 -0600

    merge SVG_middleware to dev, set up route handler to Create SVG

--- src/controllers/flamegraphController.ts
+++ src/controllers/flamegraphController.ts
@@ -1,8 +1,5 @@
 //middelware function that will take a .perf and return a .svg
-
-
 /*
-
 1. run the following command to collapse the stack frames
 ./stackcollapse-perf.pl out.perf > out.folded
 
@@ -24,57 +21,58 @@ type FlamegraphSVGController = {
   //collapse the stack traces in the perf:
   stackCollapse: (req: Request, res: Response, next: NextFunction) => void, 
   //convert the collapsed stack traces into an SVG:
-  toSVG: (req: Request, res: Response, next: NextFunction) => void, 
+  toSVG: (req: Request, res: Response, next: NextFunction) => void
 }
 
 const flamegraphController: FlamegraphSVGController = {
   stackCollapse: (req: Request, res: Response, next: NextFunction): void => {
     //node child process
-    // const fileName = parseInt(res.locals.id); 
-    const fileName = '1';
-    exec(`./src/perlScripts/stackCollapse-perf.pl ./database/captures/1.perf > ./database/captures/folded/1.folded`,
+    const fileName = res.locals.id; 
+    // const fileName = '1';
+    exec(`./src/perlScripts/stackCollapse-perf.pl ./database/captures/${fileName}.perf > ./database/folded/${fileName}.folded`,
        (error: stream.Readable, stdout: stream.Readable, stderr: stream.Readable) => {
         //change to String | Buffer if it doesnt work
         if (error) {
-          //   return next({
-          //     log: 'something went wrong with the stackFolder middleware',
-          //     message: { err: error },
-          //   });
-          console.log('err in stackCollapse');
-          return res.status(500).json('stackFolder err-----');
+            return next({
+              log: 'something went wrong with the stackFolder middleware',
+              message: { err: error },
+            });
+          // console.log('err in stackCollapse');
+          // return res.status(500).json('stackFolder err-----');
         }
         return next();
       })
   },
 
   toSVG: (req: Request, res: Response, next: NextFunction): void => {
-    // const fileName = res.locals.id;
-    const fileName = '1';
-    exec(`./src/perlScripts/flamegraph.pl ./database/captures/folded/1.folded > ./database/captures/SVGs/1.svg`,
+    const fileName = res.locals.id;
+    // const fileName = '1';
+    exec(`./src/perlScripts/flamegraph.pl ./database/folded/${fileName}.folded > ./database/SVGs/${fileName}.svg`,
       (error: Error, stdout: string | Buffer, stderr: string | Buffer) => {
       //store file ./database/SVGs
       if (!error) return next();
-      else return res.status(500).json('toSVG err-----');
-      // return next({
-      //   log: 'i am lost-------',
-      //   message: 'Error in toSVG controller'
-      // });
+      else 
+      // return res.status(500).json('toSVG err-----');
+      return next({
+        log: 'something went wrong with the toSVG middleware',
+        message: {err: error}
+      });
       
     })
   }
 }
 
 // temperaty testing of functiosn, will remove
-app.get('/', (req: Request, res: Response): Object => res.json('test the server..'))
+// app.get('/', (req: Request, res: Response): Object => res.json('test the server..'))
 
-app.get('/test',
-  flamegraphController.stackCollapse,
-  flamegraphController.toSVG,
-  (req: Request, res: Response): Object => {
-  return res.status(200).json('test fg controllers sucessfull-----');
-})
+// app.get('/test',
+//   flamegraphController.stackCollapse,
+//   flamegraphController.toSVG,
+//   (req: Request, res: Response): Object => {
+//   return res.status(200).json('test fg controllers sucessfull-----');
+// })
 
-app.listen('3000', (err:Error) => {if (err) console.log(err); else console.log('server is listening....at 3000')})
+// app.listen('3000', (err:Error) => {if (err) console.log(err); else console.log('server is listening....at 3000')})
 
 
 module.exports = flamegraphController;
--- src/routes/api.router.ts
+++ src/routes/api.router.ts
@@ -1,19 +1,26 @@
 import {NextFunction, Router, Request, Response} from 'express';
-
+const flamegraph = require('../controllers/flamegraphController')
 const apiRouter = Router();
 
-apiRouter.get('/',
+apiRouter.get('/', 
   (req: Request, res: Response, next: NextFunction) => {
     console.log(`${new Date().toLocaleString()}: apiRouter handling ${req.method} ${req.url}`);
     return next({message: 'GET /api/ not yet implemented'})
   }
 )
 
+// This middleware was used for testing
+// const will = (req: Request, res: Response, next: NextFunction) => {res.locals.id = '1'; return next()}
+
 // Create
-apiRouter.post('/captures',
+apiRouter.post('/captures', 
+  // will, /*will's middlware, */
+  flamegraph.stackCollapse,
+  flamegraph.toSVG,
   (req: Request, res: Response, next: NextFunction) => {
-    console.log(`${new Date().toLocaleString()}: apiRouter handling ${req.method} ${req.url}`);
-    next({message: 'POST /api/captures/ not yet implemented'})
+    return res.status(200).send('svg file created and stored in the /database/SVGs')
+    // what we send back to the client will depend on the front end
+    //architecture and how we want the user to see what they just uploaded
   }
 )
 
@@ -56,5 +63,8 @@ apiRouter.delete('/captures/:id',
     next({message: 'DELETE /api/captures/:id not yet implemented'})
   }
 )
+
+// define a global err handler based on the server.globalError method
+
 
 export default apiRouter;
