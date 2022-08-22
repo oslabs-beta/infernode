a927e974be74e6fad3ab624944b74f68d2a73397
hmu1540 <h.mu.1540@gmail.com>
2022-08-22 08:55:09 -0500

    Create Sidebar component

--- src/public/infernode/views/History/HistorySidebar.tsx
+++ src/public/infernode/views/History/HistorySidebar.tsx
@@ -1,10 +1,42 @@
 import React from 'react';
+import { Form } from 'react-bootstrap';
 import Card from 'react-bootstrap/Card';
+import HistoryFileItem from './HistoryFileItem';
 
-export default function HistorySidebar(): JSX.Element {
+type HistorySidebarProps = {
+  // display: (name: string) => Promise<void>
+  display: (name: string) => void
+}
+
+export default function HistorySidebar(props: HistorySidebarProps) {
+  const { display } = props;
+
+  async function getFileLists() {
+    const res = await fetch('/api/captures');
+    const data: string[] = await res.json();
+    return data;
+  }
+  // const files = getFileLists();
+  const files = ['1.svg', '2.svg', '3.svg']; // not meaningful name
+
+  // const remove = (name: string) {
+  //   //
+  // }
+
+  const historyFileLists: JSX.Element[] = [];
+  for (let i = 0; i < files.length; i++) {
+    historyFileLists.push(<HistoryFileItem name={files[i]} display={display} /* remove={remove} */ key={i} id ={i}/>);
+  }
+
   return (
-    <Card className="flex-shrink-0 align-self-start">
-      <img alt="mock history page menu" src="/mocks/history-menu.png" />
+    <Card className="align-self-start">
+      {/* <img alt="mock history page menu" src="/mocks/history-menu.png" /> */}
+      <Form>
+        <Form.Group>
+          <Form.Label>History</Form.Label>
+          {historyFileLists}
+        </Form.Group>
+      </Form>
     </Card>
   );
 }
