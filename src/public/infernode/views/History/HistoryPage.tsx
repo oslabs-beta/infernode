b87e5640a622f47e7baeaa849fc23c1807c4ae2f
hmu1540 <h.mu.1540@gmail.com>
2022-08-22 08:53:47 -0500

    Add one flamegraph to the main panel

--- src/public/infernode/views/History/HistoryPage.tsx
+++ src/public/infernode/views/History/HistoryPage.tsx
@@ -1,18 +1,30 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Stack from 'react-bootstrap/Stack';
 import Card from 'react-bootstrap/Card';
 import HistorySidebar from './HistorySidebar';
+import {Fg1, Fg2, Fg3} from './idx';
+// reference: https://openbase.com/js/@svgr/webpack
+// reference: https://react-svgr.com/docs/webpack/
 
 export default function HistoryPage(): JSX.Element {
+  // const [Flamegraph, setFlamegraph] = useState<JSX.Element>(Fg1);
+  // // console.log(Flamegraph);
+  // const display = (name: string) => {
+
+  //   const fgName = <`Fg${name}`/>;
+  //   setFlamegraph(<{fgName} />);
+  // };
+  // const remove =(name: string): void => {
+  //   // remove from the side bar and from the filesystem
+
+  // }
+const display = () => {};
+
   return (
     <Stack direction="horizontal" gap={3}>
-      <HistorySidebar />
+      <HistorySidebar display={display} />
       <Card>
-        <img alt="mock history page header" src="/mocks/history-header.png" />
-        <div style={{ width: 1024, height: 'auto' }}>
-          <embed type="image/svg+xml" src="/node-example-fg.svg" width="1024" />
-        </div>
-        <img alt="mock history page footer" src="/mocks/history-footer.png" />
+        <Fg1 />
       </Card>
     </Stack>
   );
