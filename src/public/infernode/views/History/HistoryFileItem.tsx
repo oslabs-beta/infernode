8bc07076f6d4f8e79da064a51edb8cbd6843ee8c
hmu1540 <84532849+hmu1540@users.noreply.github.com>
2022-08-24 10:27:46 -0500

    Feature/histroy page sidebar (#61)

--- src/public/infernode/views/History/HistoryFileItem.tsx
+++ src/public/infernode/views/History/HistoryFileItem.tsx
@@ -5,26 +5,23 @@ import Form from 'react-bootstrap/Form';
 type HistoryFileItemProps = {
   name: string,
   display: (event: SyntheticEvent) => void,
-  // remove: (name: string) => Promise<void>,
-  id: number,
+  remove: (event: SyntheticEvent) => void,
   date: Date
 };
 
 export default function HistoryFileItem(props: HistoryFileItemProps) {
   const {
     name,
-    display, /* remove, */
-    id,
+    display,
+    remove,
     date,
   } = props;
   return (
     <div className="flex-box">
       <Form.Text>{name}</Form.Text>
       <Form.Text>{new Date(date).toLocaleString()}</Form.Text>
-      <Button variant="primary" onClick={display} id={`display-${id}`}>Display</Button>
-      {/* <Button variant="secondary" onClick={() => remove({ name })} id={`remove-${id}`}>
-      Remove
-      </Button> */}
+      <Button variant="primary" onClick={display}>Display</Button>
+      <Button variant="secondary" onClick={remove}>Remove</Button>
     </div>
   );
 }
