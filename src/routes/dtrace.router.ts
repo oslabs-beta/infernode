c831b41e7bcafbd08d48660a836a6bdb10921c89
Luke Roberts <luke.roberts089@gmail.com>
2022-08-31 17:53:36 -0600

    restructure routers and controllers so all 3 charts can be generated with perf or dtrace

--- src/routes/dtrace.router.ts
+++ src/routes/dtrace.router.ts
@@ -2,9 +2,11 @@ import { Router, Request, Response } from 'express';
 import dbController from '../controllers/db.controller';
 import DtraceController from '../controllers/dTrace.controller';
 import flamegraphController from '../controllers/flamegraphController';
+import icicleController from '../controllers/icicle.controller';
 
 const dtraceRouter = Router();
 
+// create a new flamegraph with a dtrace
 dtraceRouter.post(
   '/',
   dbController.createEmptyRecord,
@@ -12,6 +14,32 @@ dtraceRouter.post(
   DtraceController.runDtrace,
   DtraceController.foldDtrace,
   flamegraphController.toSVG,
+  (_req: Request, res: Response) => {
+    res.status(200).send('test successful');
+  },
+);
+
+// create a new icicle chart with a dtrace
+dtraceRouter.post(
+  '/icicle',
+  dbController.createEmptyRecord,
+  DtraceController.nodeLaunch,
+  DtraceController.runDtrace,
+  DtraceController.foldDtrace,
+  icicleController.toIcicleSVG,
+  (_req: Request, res: Response) => {
+    res.status(200).send('test successful');
+  },
+);
+
+// create a new flame chart with a dtrace
+dtraceRouter.post(
+  '/flamechart',
+  dbController.createEmptyRecord,
+  DtraceController.nodeLaunch,
+  DtraceController.runDtrace,
+  DtraceController.foldDtrace,
+  // flamechartController.toFlamechartSVG,
   (_req: Request, res: Response) => {
     res.status(200).send('test successful');
   },
