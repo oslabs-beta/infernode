720b2cd558dbae63729fd70f1d33b5415d573947
Chase Walters <cwwalters@fastmail.com>
2022-09-02 11:23:39 -0600

    Add state provider to page tests

--- src/__tests__/capturePageUnit.test.tsx
+++ src/__tests__/capturePageUnit.test.tsx
@@ -1,28 +1,26 @@
-import { render, screen } from '@testing-library/react'
-import React from 'react'
+import { render, screen } from '@testing-library/react';
+import React from 'react';
 import { BrowserRouter } from 'react-router-dom';
-import CapturePage from '../public/infernode/views/Capture/CapturePage'
+import CapturePage from '../public/infernode/views/Capture/CapturePage';
+import { Provider } from 'react-redux';
+import { store } from '../public/infernode/store/store';
 
 describe('Capture page unit tests', () => {
-
   beforeEach(() => {
     const page = render(
-      <BrowserRouter>
-        <CapturePage />
-      </BrowserRouter>
-    )
-  })
+      <Provider store={store}>
+        <BrowserRouter>
+          <CapturePage />
+        </BrowserRouter>
+      </Provider>,
+    );
+  });
 
   describe('Capture page will render correct buttons', () => {
-    
     test('Capture button will render capture page', () => {
-      expect(true).toBe(true)
-    })
-    
-    test('Capture page renders correct content', () => {
-      
-    })
-    
-  })
+      expect(true).toBe(true);
+    });
 
-})
+    test('Capture page renders correct content', () => {});
+  });
+});
