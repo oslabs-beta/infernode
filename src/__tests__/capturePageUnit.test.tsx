9a8c67c2a35452e72ef9b879252c4a0bbb72b09c
hmu1540 <h.mu.1540@gmail.com>
2022-09-02 12:39:14 -0500

    Set up test file

--- src/__tests__/capturePageUnit.test.tsx
+++ src/__tests__/capturePageUnit.test.tsx
@@ -2,16 +2,20 @@ import { render, screen } from '@testing-library/react'
 import React from 'react'
 import { BrowserRouter } from 'react-router-dom';
 import CapturePage from '../public/infernode/views/Capture/CapturePage'
+import { Provider } from 'react-redux'
+import { store } from '../public/infernode/store/store'
 
 describe('Capture page unit tests', () => {
-
   beforeEach(() => {
     const page = render(
+      <Provider store={store}>
       <BrowserRouter>
         <CapturePage />
       </BrowserRouter>
+      </Provider>
     )
   })
+
 
   describe('Capture page will render correct buttons', () => {
     
