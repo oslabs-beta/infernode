683b30138d2a4279057d8d44dcda5234bdb8a33f
Chase W <cwwalters@fastmail.com>
2022-09-01 19:10:45 -0600

    ManagePage refactoring, remove action bugfix (#103)

--- src/public/infernode/views/Manage/ManagePage.tsx
+++ src/public/infernode/views/Manage/ManagePage.tsx
@@ -1,26 +1,11 @@
 import React from 'react';
-import { useNavigate } from 'react-router-dom';
 import Stack from 'react-bootstrap/Stack';
-import Button from 'react-bootstrap/Button';
-import Card from 'react-bootstrap/Card';
-import Form from 'react-bootstrap/Form';
-import axios from 'axios';
 import ManageSidebar from './ManageSidebar';
-import { useAppDispatch, useAppSelector } from '../../store/hooks';
-import {
-  setFile, updateCaptureName, updateAppName, updateCreator, updateDate, updateData, setLoading,
-  setProgress,
-} from '../../store/uploadSlice';
+import { useAppSelector } from '../../store/hooks';
 import ProgressCard from '../../components/layout/ProgressCard';
-import { fetchAllCaptures } from '../../store/captureSlice';
+import ManageForm from './ManageForm';
 
 export default function ManagePage(): JSX.Element {
-  const dispatch = useAppDispatch();
-  const navigate = useNavigate();
-  const formData = new FormData();
-  const {
-    appName, captureName, creator, date, data,
-  } = useAppSelector((state) => state.upload.capture);
   const { file, loading, progress } = useAppSelector((state) => state.upload);
 
   const submitForm = (event: React.SyntheticEvent) => {
@@ -61,42 +46,7 @@ export default function ManagePage(): JSX.Element {
   return (
     <Stack direction="horizontal" gap={3}>
       <ManageSidebar />
-      {(!loading && (
-        <Card className="w-100">
-          <Form
-            onSubmit={submitForm}
-            id="uploadForm"
-          >
-            <Form.Group controlId="captureFile" className="m-3">
-              <Form.Label>Capture file</Form.Label>
-              <Form.Control
-                type="file"
-              />
-            </Form.Group>
-            <Form.Group controlId="captureName" className="m-3">
-              <Form.Label>Capture name</Form.Label>
-              <Form.Control type="text" placeholder="trace slow function" onChange={(e) => dispatch(updateCaptureName(e.target.value))} />
-            </Form.Group>
-            <Form.Group controlId="appName" className="m-3">
-              <Form.Label>Application name</Form.Label>
-              <Form.Control type="text" placeholder="NodeApp.js" onChange={(e) => dispatch(updateAppName(e.target.value))} />
-            </Form.Group>
-            <Form.Group controlId="creator" className="m-3">
-              <Form.Label>Creator</Form.Label>
-              <Form.Control type="text" placeholder="anonymous" onChange={(e) => dispatch(updateCreator(e.target.value))} />
-            </Form.Group>
-            <Form.Group controlId="data" className="m-3">
-              <Form.Label>Metadata</Form.Label>
-              <Form.Control type="text" onChange={(e) => dispatch(updateData(e.target.value))} />
-            </Form.Group>
-            <Form.Group controlId="date" className="m-3">
-              <Form.Label>Date</Form.Label>
-              <Form.Control type="text" placeholder={new Date().toLocaleString()} onChange={(e) => dispatch(updateDate(e.target.value))} />
-            </Form.Group>
-            <Button variant="primary" type="submit" className="m-3">Upload</Button>
-          </Form>
-        </Card>
-      )) || <ProgressCard fileName={file} progress={progress} />}
+      {!loading ? <ManageForm /> : <ProgressCard fileName={file} progress={progress} />}
     </Stack>
   );
 }
