c01623668bc7f9965d376c3d03560ce8a3c92dac
Chase Walters <cwwalters@fastmail.com>
2022-09-02 10:26:18 -0600

    Add activePage state, use with navbar

--- src/public/infernode/views/Help/HelpPage.tsx
+++ src/public/infernode/views/Help/HelpPage.tsx
@@ -3,8 +3,12 @@ import Stack from 'react-bootstrap/Stack';
 import Card from 'react-bootstrap/Card';
 import { Outlet } from 'react-router-dom';
 import HelpSidebar from './HelpSidebar';
+import { useAppDispatch } from '../../store/hooks';
+import { setActivePage } from '../../store/configSlice';
 
 export default function HelpPage(): JSX.Element {
+  const dispatch = useAppDispatch();
+  dispatch(setActivePage('/help'));
   return (
     <Stack direction="horizontal" gap={3}>
       <HelpSidebar />
