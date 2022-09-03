5e241b715566150a2869a2671db03526d7ba3604
Chase Walters <cwwalters@gmail.com>
2022-09-03 13:11:38 -0600

    Move env detection to controller

--- src/controllers/controllers.module.ts
+++ src/controllers/controllers.module.ts
@@ -1,7 +1,9 @@
 import HealthController from './health.controller';
 import NotFoundController from './notfound.controller';
 import FileController from './file.controller';
+import EnvController from './env.controller';
 
+export const envController = new EnvController();
 export const fileController = new FileController();
 export const notFoundController = new NotFoundController();
 export const healthController = new HealthController(
