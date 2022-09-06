a93223574f03013a3c89b49ac345ca5fa3770805
Luke Roberts <105823811+8000meters@users.noreply.github.com>
2022-09-06 11:26:29 -0600

    Site dev (#141)

--- .eslintrc.js
+++ .eslintrc.js
@@ -5,12 +5,12 @@ module.exports = {
     tsconfigRootDir: __dirname,
     project: ['./tsconfig.json'],
   },
-  "settings": {
-    "import/resolver": {
-      "node": {
-        "extensions": [".js", ".jsx", ".ts", ".tsx"]
-      }
-    }
+  settings: {
+    'import/resolver': {
+      node: {
+        extensions: ['.js', '.jsx', '.ts', '.tsx'],
+      },
+    },
   },
   plugins: ['@typescript-eslint'],
   extends: [
@@ -18,11 +18,20 @@ module.exports = {
     'airbnb',
     'airbnb-typescript',
     'airbnb/whitespace',
-    "airbnb/hooks",
+    'airbnb/hooks',
     'plugin:@typescript-eslint/recommended',
     'plugin:@typescript-eslint/recommended-requiring-type-checking',
   ],
-  ignorePatterns: ["src/**/*.test.ts", "src/__tests__/", "dist/", "src/assets/", "coverage/", "src/public"],
+  ignorePatterns: [
+    'src/**/*.test.ts',
+    'src/__tests__/',
+    'dist/',
+    'src/assets/',
+    'coverage/',
+    'src/public',
+    'src/examples/',
+    './site/',
+  ],
   env: {
     browser: true,
     es6: true,
@@ -30,9 +39,9 @@ module.exports = {
     node: true,
   },
   rules: {
-    "import/extensions": ["error", "never"],
-    "no-console": "off",
-    "no-unused-expressions": "off",
-    "no-console": "off",
-  }
+    'import/extensions': ['error', 'never'],
+    'no-console': 'off',
+    'no-unused-expressions': 'off',
+    'no-console': 'off',
+  },
 };
