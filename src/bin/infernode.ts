ff72621c0b2905c167936a8fd401fdec2c34059f
hmu1540 <h.mu.1540@gmail.com>
2022-09-08 20:36:40 -0500

    fix(bin): start infernode in the browser

--- src/bin/infernode.ts
+++ src/bin/infernode.ts
@@ -1,5 +1,12 @@
 #!/usr/bin/env node
+import open from 'open';
+import logger from '../utils/logging';
 import infernode from '../index';
 
-console.log(`Infernode started: http://localhost:${infernode.port}/`);
-console.log('Press \'control + c\' to exit');
+// opens the url in the default browser
+open(`http://localhost:${infernode.port}/`)
+  .then(() => {
+    console.log(`Infernode started: http://localhost:${infernode.port}/`);
+    console.log('Press \'control + c\' to exit');
+  })
+  .catch((err) => logger.error(err));
