9ad407e84c557297250fb32bdfbd54ebfb3d7ce1
cdschexnide <cody.schexnider@snhu.edu>
2022-09-30 07:28:58 -0500

    fix(index.ts): modified default server port in index.ts as well as the webpack proxy target
    I updated the port number in index.ts to avoid conflicts with the use of port 3000. I also changed
    the webpack proxy target to match the change.
    
    'fix #207'

--- src/index.ts
+++ src/index.ts
@@ -8,7 +8,7 @@ import { globalErrorHandler } from './utils/globalErrorHandler';
 dotenv.config();
 
 /* If the environment variable EXPRESS_PORT is set, use that port, otherwise use 3000. */
-const port: number = Number(process.env.EXPRESS_PORT) || 3000;
+const port: number = Number(process.env.EXPRESS_PORT) || 8008;
 
 /* Instantiate a Server with the port, an array of middleware, and the HealthRouter. */
 const server = new Server(port, [apiRouter], HealthRouter, NotFoundRouter, globalErrorHandler);
